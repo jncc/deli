4d59de92ba0d029d183a6c6f4a146ba8857dcb6b
Pete Montgomery <sumo.the.cat@gmail.com>
2017-01-19 15:04:55 +0000

    dockerless eb deploy

--- app.server/settings.ts
+++ app.server/settings.ts
@@ -11,7 +11,7 @@ export function getEnvironmentSettings(env: string) {
   else {
     return {
       name: env,
-      port: 8081,
+      port: 8081,  // elastic beanstalk: the default nginx configuration forwards traffic to an upstream server named nodejs at 127.0.0.1:8081
       dir: 'built/app.client'
     };
   }
