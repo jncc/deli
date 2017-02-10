ad7484968ec889acf01db1bed7d8f5f114e7865a
Pete Montgomery <sumo.the.cat@gmail.com>
2017-02-10 16:12:21 +0000

    test es1015 on beanstalk

---
+++ app.server/data/storedQueryRepository.ts
@@ -0,0 +1,57 @@
+
+import { Query } from "../query/query";
+let randomKey = require('random-key');
+
+export class StoredQuery {
+    constructor(public key: string, public query: Query) {}
+}
+
+export class StoredQueryRepository {
+
+    private data: StoredQuery[] = [
+        { key: "may2016demo",
+            query: {
+                dataset: "s2-ard",
+                bbox:  [-15, 45, 15, 65],
+                start: new Date("2016-05-01"),
+                end:   new Date("2016-05-31"),
+            }
+        },
+        { key: "june2016demo",
+            query: {
+                dataset: "s2-ard",
+                bbox:  [-15, 45, 15, 65],
+                start: new Date("2016-06-01"),
+                end:   new Date("2016-06-31"),
+            }
+        },
+        { key: "fulldemo",
+            query: {
+                dataset: "s2-ard",
+                bbox:  [-15,45,15,65],
+                start: new Date("2014-01-01"),
+                end:   new Date("2020-01-01"),
+            }
+        },
+    ];
+
+    load(key: string): Promise<StoredQuery> {
+        return new Promise((resolve, reject) => {
+            let s = this.data.find(x => x.key == key)
+            if (s === undefined) {
+                reject(`Couldn't find query with key '${key}'`);
+            } else {
+                resolve(s);
+            }
+        });
+    }
+
+    store(query: Query): Promise<StoredQuery> {
+        return new Promise((resolve, reject) => {
+            let key = randomKey.generate(10); // e.g. chTsdNQdGc
+            let s = new StoredQuery(key, query);
+            this.data.push(s);
+            resolve(s);
+        });
+    }
+}
