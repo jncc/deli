6d3923791e516b4a4722fa5f882d04481b6064dc
Pete Montgomery <sumo.the.cat@gmail.com>
2017-05-17 12:19:02 +0100

    remove unused npm dependency

--- app.client/index.tsx
+++ app.client/index.tsx
@@ -3,12 +3,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 
-import "es6-shim";
-//import "promise/polyfill";
-import "whatwg-fetch";
+// support es5 browsers such as IE11
+import "es6-shim"; // polyfill to support es6 methods and functions
+import "whatwg-fetch"; // polyfill to support 'fetch'
 
+// import stylesheets so webpack can package them
 import "./styles/main.less";
-import "./styles/config.less";
+import "./styles/config.less"; // intended to vary per instance
 
 import { Routes } from "./routes";
 import { store } from './state/store';
