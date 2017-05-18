465caaf84f8aef05e75f679b90a4721a79205dd7
Pete Montgomery <sumo.the.cat@gmail.com>
2017-05-18 11:23:03 +0100

    correct folder for leaflet-editable typings

--- app.client/typings.d.ts
+++ app.client/typings.d.ts
@@ -3,11 +3,13 @@
 // @typings/query-string type definitions are currently incorrect (too restrictive)
 declare module "query-string";
 
-// typings to make leaflet.editable plugin work
-// declare namespace L {
-//     interface MapOptions {
-//         editable: boolean
-//     }
-// }
-// declare namespace L {
-// }
+// declare my own types for leaflet-editable plugin
+// @types/leaflet-editable currently causes compile errors
+declare namespace L {
+    interface MapOptions {
+        editable: boolean;
+    }
+    interface Rectangle {
+        enableEdit(): void;
+    }
+}
--- app.server/typings.d.ts
+++ app.server/typings.d.ts
@@ -1,13 +1,3 @@
 
 declare module "random-key";
 
-// declare my own types for leaflet-editable plugin
-// @types/leaflet-editable currently causes compile errors
-declare namespace L {
-    interface MapOptions {
-        editable: boolean;
-    }
-    interface Rectangle {
-        enableEdit(): void;
-    }
-}
