d65d4cd7e38d7b439e42a0eb61ff660eb201df05
Pete Montgomery <sumo.the.cat@gmail.com>
2017-05-31 12:16:40 +0100

    finish wms modal

--- app.client/components/shared/WmsModal.tsx
+++ app.client/components/shared/WmsModal.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react'
-import { Container, Modal, Button, Header, ModalProps, Segment, Input, Label, Icon, Form, Reveal } from "semantic-ui-react";
+import { Container, Modal, Button, Header, ModalProps, Segment, Input, Label, Icon, Form, Reveal, Grid, Divider } from "semantic-ui-react";
 import * as CopyToClipboard from 'react-copy-to-clipboard';
 import * as _ from 'lodash';
 
@@ -33,37 +33,48 @@ export class WmsModal extends React.Component<WmsModalProps, WmsState> {
   }
 
   getContent() {
-    let url = this.props.wms.base_url + '::' + this.props.wms.name
 
-    return <Modal.Content>
-      Use this WMS link in your GIS client.
-      {/*<Form  >*/}
-        <Form.Group inline>
-          <Form.Field>
-            <Label size='large'>
-              {url}
-            </Label>
-          </Form.Field>
-          <Form.Field>
+    let url = this.props.wms.base_url + '?service=wms&version=1.3.0&request=GetCapabilities' // + this.props.wms.name
+
+    return (
+      <Modal.Content>
+        <Segment basic>
+          <div className='spaced slightly'>
+          <Label size='large'> {url} </Label>
+          </div>
+          <div>
             <CopyToClipboard text={url} onCopy={() => this.handleCopied()}>
                 <Button icon='copy' labelPosition='right' color='pink' content='Copy to clipboard' />
             </CopyToClipboard>
-          </Form.Field>
-          <Form.Field>
-
-            {this.state.copiedToClipboard && <span><Icon name='checkmark' />Now paste the link into your GIS client.</span>}
-          </Form.Field>
-        </Form.Group>
-      {/*</Form>*/}
-      <p>
-        You can use this link in <a href='http://www.qgis.org/en/docs' target='_blank'>QGIS</a> or <a href='https://www.arcgis.com/features/index.html' target='_blank'>ArcGIS</a>.
-      </p>
-    </Modal.Content>
+            {this.state.copiedToClipboard &&
+              <span><Icon name='checkmark'/>Copied! Now paste the WMS link into your GIS client</span>
+            }
+          </div>
+        </Segment>
+        <Segment basic>
+          <Header>You can use this link in your GIS client</Header>
+        </Segment>
+        <Grid divided stackable centered columns='2'>
+          <Grid.Column>
+            <Header icon as='h4'>
+              <Icon name='rocket' />
+              See how to use WMS <a href='http://www.qgis.org/en/docs' target='_blank'>in QGIS</a>
+            </Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Header icon as='h4'>
+              <Icon name='world' />
+              See how to use WMS <a href='https://www.arcgis.com/features/index.html' target='_blank'>in ArcGIS</a>
+            </Header>
+          </Grid.Column>
+        </Grid>
+      </Modal.Content>
+    )
   }
 
   handleCopied = () => {
     this.setState({ copiedToClipboard: true })
-    let delayed = _.debounce(() => this.setState({ copiedToClipboard: false }), 4000)
+    let delayed = _.debounce(() => this.setState({ copiedToClipboard: false }), 10000)
     delayed();
   }
 }
