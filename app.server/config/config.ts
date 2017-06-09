91de6a82cdfe1e31bd5407e19e42614d18978aab
Pete Montgomery <sumo.the.cat@gmail.com>
2017-06-09 13:16:32 +0100

    work on deploy

---
+++ app.server/config/config.ts
@@ -0,0 +1,10 @@
+
+/** This file gets overwritten in a production build with one of the
+ *  tenant-specific config in this directory */
+
+import { ServerConfig } from './ServerConfig'
+
+// feel free to change the below import to switch between tenant configs at dev time
+//import { config as c}  from './config.lidar'
+
+//export const config: ServerConfig = c
