2085db5155b9052ba682261137f589a95009c1c4
Pete Montgomery <sumo.the.cat@gmail.com>
2017-06-15 14:04:01 +0100

    tidy

--- app.client/components/products/Map.tsx
+++ app.client/components/products/Map.tsx
@@ -21,19 +21,19 @@ interface MapProps {
 
 export class Map extends React.Component<MapProps, {}> {
 
-  map: L.Map;
-  footprintLayerGroup: L.LayerGroup;
-  visualLayerGroup: L.LayerGroup;
+  map: L.Map
+  footprintLayerGroup: L.LayerGroup
+  visualLayerGroup: L.LayerGroup
 
   // tuples of { product, footprint, wms }
   // associating a product with its corresponding leaflet map objects
   productTuples: { product: Product,
                    footprint: L.GeoJSON,
-                   wms: L.TileLayer.WMS | undefined }[] = [];
+                   wms: L.TileLayer.WMS | undefined }[] = []
 
   render() {
     // react has nothing to do with the leaflet map; all events will be handled manually
-    return <div className='map'></div>;
+    return <div className='map'></div>
   }
 
   componentDidMount() {
@@ -64,31 +64,28 @@ export class Map extends React.Component<MapProps, {}> {
       ],
       attributionControl: false,
       editable: true // enable leaflet.editable plugin
-    });
+    })
 
     map.setView(config.map.defaultCenter, config.map.defaultZoom)
 
     // add layer groups
-    this.footprintLayerGroup = L.layerGroup([]).addTo(map);
-    this.visualLayerGroup = L.layerGroup([]).addTo(map);
+    this.footprintLayerGroup = L.layerGroup([]).addTo(map)
+    this.visualLayerGroup = L.layerGroup([]).addTo(map)
 
     // add the bbox rectangle
-    let bboxRect = L.rectangle(bboxFlatArrayToCoordArray(this.props.query.bbox), { fillOpacity: 0 });
-    bboxRect.addTo(map);
-    bboxRect.enableEdit(); // enable a moveable bbox with leaflet.editable
+    let bboxRect = L.rectangle(bboxFlatArrayToCoordArray(this.props.query.bbox), { fillOpacity: 0 })
+    bboxRect.addTo(map)
+    bboxRect.enableEdit() // enable a moveable bbox with leaflet.editable
 
     // update the query state when the bbox is altered
     map.on('editable:vertex:dragend', (e: any) => {
         if (e.layer === bboxRect) { // e.layer property added by leaflet.editable
           let b = bboxRect.getBounds()
-          let bbox = [
-            roundTo3Decimals(b.getWest()),
-            roundTo3Decimals(b.getSouth()),
-            roundTo3Decimals(b.getEast()),
-            roundTo3Decimals(b.getNorth())];
-          this.props.queryChanged(Object.assign({}, this.props.query, { bbox: bbox }));
+          let bbox = [b.getWest(), b.getSouth(), b.getEast(), b.getNorth()]
+            .map(roundTo3Decimals)
+          this.props.queryChanged(Object.assign({}, this.props.query, { bbox: bbox }))
         }
-    });
+    })
   }
 
   updateProductList() {
@@ -98,7 +95,7 @@ export class Map extends React.Component<MapProps, {}> {
         product: p,
         footprint: this.makeProductFootprintLayer(p),
         wms: this.makeProductWmsLayer(p),
-      }));
+      }))
   }
 
   addProductsToMap() {
@@ -121,33 +118,33 @@ export class Map extends React.Component<MapProps, {}> {
     // WMS layers on and off rather than just showing them all
     this.props.result.collections.forEach(c => {
       if (c.data.wms) {
-        let wmsUrl = c.data.wms.base_url;
+        let wmsUrl = c.data.wms.base_url
         let wmsOptions = {
           layers: c.data.wms.name,
           format: 'image/png',
           transparent: true
-        };
-        let layer = L.tileLayer.wms(wmsUrl, wmsOptions);
-        this.visualLayerGroup.addLayer(layer);
+        }
+        let layer = L.tileLayer.wms(wmsUrl, wmsOptions)
+        this.visualLayerGroup.addLayer(layer)
       }
-    });
+    })
   }
 
   makeProductFootprintLayer(p: Product) {
 
-    let footprint = L.geoJSON(p.footprint, productFootprintStyleOff);
+    let footprint = L.geoJSON(p.footprint, productFootprintStyleOff)
 
     footprint.on('mouseout', () => {
-      footprint.setStyle(() => productFootprintStyleOff);
-      this.props.productUnhovered(p);
-    });
+      footprint.setStyle(() => productFootprintStyleOff)
+      this.props.productUnhovered(p)
+    })
 
     footprint.on('mouseover', () => {
-      footprint.setStyle(() => productFootprintStyleOn);
-      this.props.productHovered(p);
-    });
+      footprint.setStyle(() => productFootprintStyleOn)
+      this.props.productHovered(p)
+    })
 
-    return footprint;
+    return footprint
   }
 
   makeProductWmsLayer(p: Product) {
@@ -169,23 +166,23 @@ export class Map extends React.Component<MapProps, {}> {
 
   updateHoveredProductOnMap(prev: Product | undefined, current: Product | undefined) {
     // highlight the currently hovered product
-    let hoveredTuple = this.productTuples.find(x => x.product === current);
+    let hoveredTuple = this.productTuples.find(x => x.product === current)
     if (hoveredTuple) {
-      hoveredTuple.footprint.setStyle(() => productFootprintStyleOn);
+      hoveredTuple.footprint.setStyle(() => productFootprintStyleOn)
     }
     // unhighlight the previously hovered product
     if (prev) {
-      let unhoveredTuple = this.productTuples.find(x => x.product === prev);
+      let unhoveredTuple = this.productTuples.find(x => x.product === prev)
       if (unhoveredTuple) {
-        unhoveredTuple.footprint.setStyle(() => productFootprintStyleOff);
+        unhoveredTuple.footprint.setStyle(() => productFootprintStyleOff)
       }
     }
   }
 }
 
 // can't get className to work right now, so use literal styles
-const productFootprintStyleOff = { fillOpacity: 0, weight: 1, color: '#666' };
-const productFootprintStyleOn =  { fillOpacity: 0, weight: 3, color: '#555' };
+const productFootprintStyleOff = { fillOpacity: 0, weight: 1, color: '#666' }
+const productFootprintStyleOn =  { fillOpacity: 0, weight: 3, color: '#555' }
 
 /** Rounds the number to 3 decimal places. */
-const roundTo3Decimals = (n: number) => Math.round(1000 * n) / 1000;
+const roundTo3Decimals = (n: number) => Math.round(1000 * n) / 1000
